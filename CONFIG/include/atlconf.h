f5655eb5e856401baf96e83daba098c24275c263
Debian Science Team <debian-science-maintainers@lists.alioth.debian.org>
2013-05-26 16:47:51 +0000

    _alpha
    ===================================================================
    
    Gbp-Pq: Name 18_alpha.diff

--- CONFIG/include/atlconf.h
+++ CONFIG/include/atlconf.h
@@ -65,6 +65,13 @@ enum MACHTYPE {MACHOther, IbmPwr3, IbmPwr4, IbmPwr5, PPCG4, PPCG5,
 #define MachIsHPPA(mach_) \
    ( 0 )
 #endif
+#ifdef __alpha__
+#define MachIsALPHA(mach_) \
+  ( __alpha__ )
+#else
+#define MachIsALPHA(mach_) \
+  ( 0 )
+#endif
 
 
 static char *f2c_namestr[5] = {"UNKNOWN","Add_", "Add__", "NoChange", "UpCase"};
